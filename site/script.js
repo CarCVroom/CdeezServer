ea5cf8fd37a958a46405f7e269f628b43d69d009
CarCVroom <bjornarlundm@gmail.com>
2026-03-05 20:22:53 +0100

    Initial commit

---
+++ site/script.js
@@ -0,0 +1,6 @@
+function test() {
+        let username = document.getElementById('username').value;
+        let password = document.getElementById('password').value;
+
+        console.log(username, password);
+}
