ec4df7d5df004e9735c96cad27b02110cabefa7c
CarCVroom <bjornarlundm@gmail.com>
2026-03-19 10:00:58 +0100

    calculator
    its still a server

--- server/gui/ctyoes.py
+++ server/gui/ctyoes.py
@@ -15,29 +15,55 @@ root = tk.Tk()
 root.title("simple calulator")
 root.geometry("900x900")
 
-
-# Gets all of the numbers and operator
-entry = tk.Entry(root)
-entry.place(relx=0.5, rely=0.4, anchor="center")
-
-entry2 = tk.Entry(root)
-entry2.place(relx=0.5, rely=0.46, anchor="center")
-
-entry3 = ttk.Combobox(root, values=["+", "-", "*", "/"]) 
-entry3.place(relx=0.5, rely=0.43, anchor="center")
-
-def math():
-        value1 = int(entry.get())
-        value2 = int(entry2.get())
-        value3 = ord(entry3.get()[0])
+one = tk.Button(root, height=10,width=20,text="1")
+one.place(relx=0.32, rely=0.1, anchor="center")
         
-        resualt = lib.onePlusOne(value1, value2, value3)
-        label.config(text=f'The answer is {resualt}')
+two = tk.Button(root, height=10, width=20,text="2")
+two.place(relx=0.5, rely=0.1, anchor="center")
 
-button = tk.Button(root, text="Submit", command=math)
-button.place(relx=0.5, rely=0.5, anchor="center")
+three = tk.Button(root, height=10, width=20,text="3")
+three.place(relx=0.68, rely=0.1, anchor="center")
 
-label = tk.Label(root, text="")
-label.place(relx=0.5, rely=0.55, anchor="center")
+four = tk.Button(root, height=10,width=20,text="4")
+four.place(relx=0.32, rely=0.28, anchor="center")
+        
+five = tk.Button(root, height=10, width=20,text="5")
+five.place(relx=0.5, rely=0.28, anchor="center")
+
+six = tk.Button(root, height=10, width=20,text="6")
+six.place(relx=0.68, rely=0.28, anchor="center")
+
+seven = tk.Button(root, height=10,width=20,text="7")
+seven.place(relx=0.32, rely=0.46, anchor="center")
+        
+eight = tk.Button(root, height=10, width=20,text="8")
+eight.place(relx=0.5, rely=0.46, anchor="center")
+
+nine = tk.Button(root, height=10, width=20,text="9")
+nine.place(relx=0.68, rely=0.46, anchor="center")
+
+zero = tk.Button(root, height=10, width=20,text="8")
+zero.place(relx=0.5, rely=0.64, anchor="center")
+
+equal = tk.Button(root, height=10, width=20,text="=")
+equal.place(relx=0.68, rely=0.64, anchor="center")
+
+pluss = tk.Button(root, height=10, width=20,text="+")
+pluss.place(relx=0.32, rely=0.64, anchor="center")
+
+divison = tk.Button(root, height=10, width=20,text="/")
+divison.place(relx=0.5, rely=0.82, anchor="center")
+
+minus = tk.Button(root, height=10, width=20,text="-")
+minus.place(relx=0.68, rely=0.82, anchor="center")
+
+multi = tk.Button(root, height=10, width=20,text="*")
+multi.place(relx=0.32, rely=0.82, anchor="center")
+
+#button = tk.Button(root, text="Submit", command=math)
+#button.place(relx=0.5, rely=0.5, anchor="center")
+
+label = tk.Label(root, text="test")
+label.place(relx=0.5, rely=0.95, anchor="center")
 
 root.mainloop()
